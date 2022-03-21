685172243e6862b6f9cc5f7567c6ec7f73e410be
Virag Kormoczy <vkormoczy@gmail.com>
2022-03-21 17:28:41 +0200

    Add luxon file and functionality

--- modules/index.js
+++ modules/index.js
@@ -1,7 +1,79 @@
-import Book from "./book.js";
+import Book from './book.js';
 
-import { navigate } from "./nav-items.js";
+import navigate from './nav-items.js';
 
-import { time } from "./date.js";
+import { luxon } from './luxon.js';
 
-import { libraryFunction } from "./create-book.js";
+const bookSection = document.querySelector('.book-list');
+const bookTitle = document.querySelector('#title');
+const bookAuthor = document.querySelector('#author');
+const addBtn = document.querySelector('#submit');
+
+window.addEventListener('DOMContentLoaded', () => {
+  class Library {
+    constructor() {
+      this.library = JSON.parse(localStorage.getItem('book-collection')) || [];
+    }
+
+    addBook(bookTitle, bookAuthor) {
+      const selectedBook = new Book(bookTitle.value, bookAuthor.value);
+      this.library.push(selectedBook);
+      this.createBook();
+    }
+
+    createBook() {
+      bookSection.innerHTML = '';
+      for (let i = 0; i < this.library.length; i += 1) {
+        const bookContainer = document.createElement('div');
+        bookContainer.setAttribute('class', 'container');
+        const bookDescription = document.createElement('h2');
+        bookDescription.setAttribute('class', 'width');
+        const deleteBtn = document.createElement('button');
+        deleteBtn.setAttribute('class', 'deletebtn');
+        deleteBtn.setAttribute('data', i);
+
+        bookDescription.textContent = `${this.library[i].title} by ${this.library[i].author}`;
+        deleteBtn.textContent = 'Delete';
+
+        bookContainer.appendChild(bookDescription);
+        bookContainer.appendChild(deleteBtn);
+        bookSection.appendChild(bookContainer);
+      }
+      this.deleteBook();
+    }
+
+    deleteBook() {
+      [...document.querySelectorAll('.deletebtn')].forEach((element) => {
+        const elementIndex = parseInt(element.getAttribute('data'), 10);
+        element.addEventListener('click', () => {
+          this.library.splice(elementIndex, 1);
+          localStorage.setItem('book-collection', JSON.stringify(this.library));
+          this.createBook();
+        });
+      });
+    }
+  }
+
+  const myLibrary = new Library();
+
+  myLibrary.createBook();
+
+  addBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (bookTitle.value === '' || bookAuthor.value === '') {
+      return;
+    }
+    myLibrary.addBook(bookTitle, bookAuthor);
+    localStorage.setItem('book-collection', JSON.stringify(myLibrary.library));
+    bookTitle.value = '';
+    bookAuthor.value = '';
+  });
+  navigate();
+});
+
+function time() {
+  const siteDate = document.querySelector('#date');
+  siteDate.textContent = luxon.DateTime.local();
+}
+
+setInterval(time, 1000);
--- modules/nav-items.js
+++ modules/nav-items.js
@@ -1,35 +1,35 @@
-const bookList = document.querySelector(".book-list");
-const header = document.querySelector(".header");
-const newBook = document.querySelector(".add-new");
-const contact = document.querySelector(".contact-section");
-const titleH1 = document.querySelector("h1");
-const navItems = Array.from(document.querySelectorAll(".navItems")[0].children);
+const bookList = document.querySelector('.book-list');
+const header = document.querySelector('.header');
+const newBook = document.querySelector('.add-new');
+const contact = document.querySelector('.contact-section');
+const titleH1 = document.querySelector('h1');
+const navItems = Array.from(document.querySelectorAll('.navItems')[0].children);
 
-export function navigate(key) {
+export default function navigate(key) {
   switch (key) {
-    case "title-h1":
-      bookList.classList.remove("hide");
-      header.classList.remove("hide");
-      newBook.classList.add("hide");
-      contact.classList.add("hide");
+    case 'title-h1':
+      bookList.classList.remove('hide');
+      header.classList.remove('hide');
+      newBook.classList.add('hide');
+      contact.classList.add('hide');
       break;
-    case "list":
-      bookList.classList.remove("hide");
-      header.classList.remove("hide");
-      newBook.classList.add("hide");
-      contact.classList.add("hide");
+    case 'list':
+      bookList.classList.remove('hide');
+      header.classList.remove('hide');
+      newBook.classList.add('hide');
+      contact.classList.add('hide');
       break;
-    case "add-new":
-      bookList.classList.add("hide");
-      header.classList.add("hide");
-      newBook.classList.remove("hide");
-      contact.classList.add("hide");
+    case 'add-new':
+      bookList.classList.add('hide');
+      header.classList.add('hide');
+      newBook.classList.remove('hide');
+      contact.classList.add('hide');
       break;
-    case "contact-section":
-      bookList.classList.add("hide");
-      header.classList.add("hide");
-      newBook.classList.add("hide");
-      contact.classList.remove("hide");
+    case 'contact-section':
+      bookList.classList.add('hide');
+      header.classList.add('hide');
+      newBook.classList.add('hide');
+      contact.classList.remove('hide');
       break;
     default:
       break;
@@ -37,11 +37,11 @@ export function navigate(key) {
 }
 
 navItems.forEach((item) => {
-  item.addEventListener("click", (e) => {
+  item.addEventListener('click', (e) => {
     navigate(e.target.parentElement.id);
   });
 });
 
-titleH1.addEventListener("click", (e) => {
+titleH1.addEventListener('click', (e) => {
   navigate(e.target.id);
 });
